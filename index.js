103116eee4f5efa6d39c6853d073c0f31d81f405
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-01-21 13:39:55 -0500

    moved to synchronous

--- index.js
+++ index.js
@@ -7,29 +7,28 @@ const configuration = new Configuration({
   apiKey: process.env.O,
 });
 const openai = new OpenAIApi(configuration);
-
-(async function () {
-    const completion = await openai.createCompletion({
+openai.createCompletion({
         model: "text-davinci-003",
         prompt: "write a random inspirational quote with the author",
         max_tokens: 4000
+      }).then(completion => {
+        quote = completion.data.choices[0].text.split('-');
+        notifier.notify({
+          title: "Daily Quote",
+          message: quote[0].trim() + ' -' + quote[1].trim()
+        });
+        if (process.env.D) {
+         openai.createCompletion({
+              model: "text-davinci-003",
+              prompt: "write a random fact",
+              max_tokens: 4000
+            }).then(fact => {
+                notifier.notify(
+                    {
+                      title: 'Daily Fact',
+                      message: fact.data.choices[0].text.trim(),
+                    }
+                  );
+            })
+        }
       });
-      quote = completion.data.choices[0].text.split('-');
-      notifier.notify({
-        title: "Daily Quote",
-        message: quote[0].trim() + ' -' + quote[1].trim()
-      });
-      if (process.env.D) {
-        const fact = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: "write a random fact",
-            max_tokens: 4000
-          });
-          notifier.notify(
-          {
-            title: 'Daily Fact',
-            message: fact.data.choices[0].text.trim(),
-          }
-        );
-      }
-})()
